ce76b8e4d81c19b4dd9c34d3f055c870b66f9a1b
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-05 21:15:12 +0300

    Fiver App With Nodejs

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -1,5 +1,62 @@
-const deleteUser = (req, res) => {
-  res.send("Hey itsss user route");
+import Gig from "../models/gig.model.js";
+import createError from "../utils/createError.js";
+
+export const createGig = async (req, res, next) => {
+  if (!req.isSeller) return next(createError("You are not a seller", 401));
+
+  const newGig = new Gig({
+    userId: req.userId,
+    ...req.body,
+  });
+
+  try {
+    const savedGig = await newGig.save();
+    res.status(201).json(savedGig);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export default deleteUser;
+export const deleteGig = async (req, res, next) => {
+  try {
+    const gig = await Gig.findById(req.params.id);
+    if (gig.userId !== req.userId)
+      return next(createError("You are not the owner of this gig", 403));
+
+    await Gig.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Gig deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getGig = async (req, res, next) => {
+  try {
+    const gig = await Gig.findById(req.params.id);
+    if (!gig) return next(createError("Gig not found", 404));
+    res.status(200).send(gig);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getGigs = async (req, res, next) => {
+  const q = req.query;
+  const filters = {
+    ...(q.userId && { userId: q.userId }),
+    ...(q.cat && { cat: q.cat }),
+    ...((q.min || q.max) && {
+      price: {
+        ...(q.min && { $gt: q.min }),
+        ...(q.max && { $lt: q.max }),
+      },
+    }),
+    ...(q.search && { title: { $regex: q.search, $options: "i" } }),
+  };
+  try {
+    const gigs = await Gig.find(filters).sort({ [q.sort]: -1 });
+    res.status(200).send(gigs);
+  } catch (err) {
+    next(err);
+  }
+};
