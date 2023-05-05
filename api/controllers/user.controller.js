0f6bb32d0de9d2d15b92097748c2346396ab9d00
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-05 19:18:39 +0300

    Fiver App With Nodejs

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,19 +1,13 @@
-import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
 export const deleteUser = async (req, res) => {
   const user = await User.findById(req.params.id);
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).send("You are not authenticated");
 
-  jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
-    if (err) return res.status(403).send("You are not authorized");
-    if (payload.id !== user._id.toString()) {
-      return res.status(403).send("You are not authorized");
-    }
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).send("User deleted successfully");
-  });
+  if (req.userId !== user._id.toString()) {
+    return res.status(403).send("You are not authorized");
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("User deleted successfully");
 };
 
 export const getUser = async (req, res, next) => {
