1563f7d169c22d3fe7e66263526591364fbb6f3c
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-05 20:16:43 +0300

    Fiver App With Nodejs

--- api/server.js
+++ api/server.js
@@ -9,6 +9,8 @@ import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
 
 const connect = async () => {
@@ -23,6 +25,7 @@ const connect = async () => {
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
