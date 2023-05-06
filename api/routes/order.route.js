4cf16d7cf4a766643fcec1ef4f06de4366e51d92
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-06 19:09:46 +0300

    Fiver App With Nodejs

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { createOrder, getOrders } from "../controllers/order.controller.js";
+import { verifyToken } from "../middlewares/auth.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, createOrder);
+router.get("/", verifyToken, getOrders);
 
 export default router;
