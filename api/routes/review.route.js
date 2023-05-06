306c7c7e984faf4a533fec00c6411a47965f7f11
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-06 18:47:47 +0300

    Fiver App With Nodejs

--- api/routes/review.route.js
+++ api/routes/review.route.js
@@ -9,7 +9,7 @@ import {
 const router = express.Router();
 
 router.post("/", verifyToken, createReview);
-router.get("/:id", getReviews);
+router.get("/:gigId", getReviews);
 router.delete("/:id", deleteReview);
 
 export default router;
