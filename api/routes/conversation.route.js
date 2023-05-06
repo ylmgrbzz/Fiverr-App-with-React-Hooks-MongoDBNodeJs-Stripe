1e7287710c26087a714f0d0871088fbfd4c44170
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-06 19:54:49 +0300

    Fiver App With Nodejs

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,5 +1,16 @@
 import express from "express";
+import {
+  getConversations,
+  createConversation,
+  getSingleConversation,
+  updateConversation,
+} from "../controllers/conversation.controller.js";
+import { verifyToken } from "../middlewares/auth.js";
 
 const router = express.Router();
+router.get("/", verifyToken, getConversations);
+router.post("/", verifyToken, createConversation);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.put("/:id", verifyToken, updateConversation);
 
 export default router;
