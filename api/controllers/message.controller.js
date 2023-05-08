f0ac5d95d56e353f56806671dabb0d6a478231b7
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-08 06:04:56 +0300

    Fiver App With Nodejs

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -1,5 +1,38 @@
-const deleteUser = (req, res) => {
-  res.send("Hey itsss user route");
+import createError from "../utils/createError.js";
+import Message from "../models/message.model.js";
+import Conservation from "../models/conservation.model.js";
+
+export const createMessage = async (req, res, next) => {
+  const newMessage = new Message({
+    conservationId: req.body.conservationId,
+    userId: req.userId,
+    desc: req.body.desc,
+  });
+
+  try {
+    const savedMessage = await newMessage.save();
+    await Conservation.findOneAndUpdate(
+      { id: req.body.conservationId },
+      {
+        $set: {
+          readBySeller: req.isSeller,
+          readByBuyer: !req.isSeller,
+          lastMessage: req.body.desc,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).send(savedMessage);
+  } catch (error) {
+    next(createError(500, error.message));
+  }
 };
 
-export default deleteUser;
+export const getMessages = async (req, res, next) => {
+  try {
+    const messages = await Message.find({ conversationId: req.params.id });
+    res.status(200).send(messages);
+  } catch (error) {
+    next(createError(500, error.message));
+  }
+};
--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { createMessage, getMessages } from "../controllers/message.controller";
+import { verifyToken } from "../middlewares/auth.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, createMessage);
+router.get("/:id", verifyToken, getMessages);
 
 export default router;
